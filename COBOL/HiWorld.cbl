e0284e94924a8e17db330ad98ba37d759fc406e3
John Bonnechappe <108032685+JohnBonnechappe@users.noreply.github.com>
2025-04-16 08:46:41 +1000

    Update HiWorld.cbl

--- COBOL/HiWorld.cbl
+++ COBOL/HiWorld.cbl
@@ -1,10 +1,12 @@
 IDENTIFICATION DIVISION.
-  AUTHOR: John Braddock
+  PROGRAM-ID. HiWorld.
+  AUTHOR. John Braddock
 ENVIRONMENT DIVISION.
 DATA DIVISION.
 WORKING-STORAGE SECTION.
     01  exit_sts        PIC 99.
 PROCEDURE DIVISION.
   main.
-  DISPLAY "Hi World".
-  STOP RUN.
+    DISPLAY "Hi World"
+    STOP RUN
+    .
